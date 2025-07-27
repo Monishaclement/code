ce5e5ec4dc3a561d9ffe55b3a90ae831157ab830
Monisha Clement <monishaclement06@gmail.com>
2025-07-27 13:23:12 +0530

    Add Kadane's Algorithm in Java

--- kadanes.java
+++ kadanes.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class kadanes {
         public static void main(String[]args)
 	{
+        int start =0;
+        int end =0;
+        int tempstart =0;
 	Scanner sc = new Scanner(System.in);
 	int n = sc.nextInt();
 	int arr[]=new int[n];
@@ -15,13 +18,24 @@ int currsum = 0;
 for(int i=0;i<arr.length;i++)
 {
 	currsum += arr[i];
-	maxsum = Math.max(maxsum, currsum);
+    if(currsum>maxsum)
+    {
+        maxsum=currsum;
+        start = tempstart;
+        end=i;
+    }
 	if(currsum<0)
 	{
 		currsum=0;
+        tempstart = i+1;
 	}
-	
 }
+    for(int i =start;i<=end;i++)
+    {
+        System.out.print(arr[i]+" ");
+    }
+	
+
 System.out.print(maxsum);
 
     }
